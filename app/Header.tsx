7df805f98bed04793077696314959aaad783f111
rajaaltus <rajaaltus@gmail.com>
2023-07-11 22:51:21 +0700

    header updated

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,8 +5,8 @@ import { usePathname } from "next/navigation";
 const Header = () => {
   const pathname = usePathname();
   return (
-    <div className="sticky top-8 w-full max-w-7xl mx-auto flex items-center justify-start h-16  border border-gray-700 px-8 mt-8 rounded-2xl">
-      <ul className="flex items-center justify-start space-x-8">
+    <div className="sticky top-0 z-50 backdrop-blur-xl shadow-2xl w-full  mx-auto flex items-center justify-start h-16  border-b border-gray-700 px-8 mt-8 ">
+      <ul className="flex items-center mx-auto max-w-7xl space-x-8">
         <li
           className={`${
             pathname === "/"
